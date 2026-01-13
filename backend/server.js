2e399c166113db429bfc1f35bc70c040b1632d51
Prachi Birle <prachi280503@gmail.com>
2026-01-13 22:06:23 +0530

    fix start script

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, "frontend")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+app.listen(PORT, () => console.log("Server running on " + PORT));
+
+
