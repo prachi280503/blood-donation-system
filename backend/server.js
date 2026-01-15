4846cb0adb9ed3130bf4b16ba6dad1f689f48fe6
Prachi Birle <prachi280503@gmail.com>
2026-01-15 10:18:56 +0530

    Fix server port and start command

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB
-mongoose.connect("mongodb+srv://prachi:prachi123@cluster0.vapkmrc.mongodb.net/bloodDB?appName=Cluster0")
+mongoose.connect("mongodb+srv://prachi:prachi123@clustemonr0.vapkmrc.mongodb.net/bloodDB?appName=Cluster0")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -22,8 +22,10 @@ app.use(express.static(path.join(__dirname, "frontend")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log("Server running on " + PORT));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
